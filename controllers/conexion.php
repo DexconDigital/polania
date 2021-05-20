cebe308afe49a10168798ea4479d6cb8038e011d
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-05-20 16:19:36 -0500

    Update conexion.php

--- controllers/conexion.php
+++ controllers/conexion.php
@@ -6,7 +6,7 @@ function Conect() {
     */
     try {
         //$echo2 = new PDO('mysql:host=50.62.209.73;port=3306;dbname=TodoOriente_noticias', 'todooriente_user', 'nosequeponer123');
-        $echo2 = new PDO('mysql:host=localhost;port=3306;dbname=2018noticias', 'root', '');
+        $echo2 = new PDO('mysql:host=localhost;port=3306;dbname=dexcondigital_noticias_inmueble', 'root', '');
         $echo2->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         return $echo2;
 
